42bd9a673eb28c4b188f91c3b49cb5500bd726f0
Ahmed Elhosiny <ahmedelhosiny842@gmail.com>
2025-07-30 18:24:58 +0300

    remove unnecessary usings

--- Exam System/Subject.cs
+++ Exam System/Subject.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Exam_System
+﻿namespace Exam_System
 {
     public class Subject
     {
